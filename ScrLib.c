f9c5d66096dd34b459543489883ed2faf209a112
skellix <24k911@gmail.com>
2015-01-09 17:32:38 -0700

    updated ScrLib.c with correct mappings

--- ScrLib.c
+++ ScrLib.c
@@ -2,46 +2,46 @@
 #include <jni.h>
 #include <ncurses.h>
 
-JNIEXPORT void JNICALL Java_ScrLib_init
+JNIEXPORT void JNICALL Java_scrlib_ScrLib_init
   (JNIEnv * env, jclass claz) {
   init();
 }
 
-JNIEXPORT void JNICALL Java_ScrLib_endwin
+JNIEXPORT void JNICALL Java_scrlib_ScrLib_endwin
   (JNIEnv * env, jclass claz) {
   endwin();
 }
 
-JNIEXPORT void JNICALL Java_ScrLib_mvprint
+JNIEXPORT void JNICALL Java_scrlib_ScrLib_mvprint
   (JNIEnv * env, jobject claz, jint x, jint y, jstring s) {
   const char* utfStr = (*env)->GetStringUTFChars(env, s, 0);
   mvprint((int) y, (int) x, s);
   (*env)->ReleaseStringUTFChars(env, s, utfStr);
 }
 
-JNIEXPORT void JNICALL Java_ScrLib_clear
+JNIEXPORT void JNICALL Java_scrlib_ScrLib_clear
   (JNIEnv * env, jobject claz) {
   clear();
 }
 
-JNIEXPORT jint JNICALL Java_ScrLib_getch
+JNIEXPORT jint JNICALL Java_scrlib_ScrLib_getch
   (JNIEnv * env, jobject claz){
   return (long) getch();
 }
 
-JNIEXPORT void JNICALL Java_ScrLib_refresh
+JNIEXPORT void JNICALL Java_scrlib_ScrLib_refresh
   (JNIEnv * env, jobject claz) {
   refresh();
 }
 
-JNIEXPORT jint JNICALL Java_ScrLib_scrWidth
+JNIEXPORT jint JNICALL Java_scrlib_ScrLib_scrWidth
   (JNIEnv * env, jobject claz) {
   int width, height;
   getmaxyx(stdscr, width, height);
   return (long) width;
 }
 
-JNIEXPORT jint JNICALL Java_ScrLib_scrHeight
+JNIEXPORT jint JNICALL Java_scrlib_ScrLib_scrHeight
   (JNIEnv * env, jobject claz) {
   int width, height;
   getmaxyx(stdscr, width, height);
